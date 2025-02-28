ce8fb55c1d21429968525802ca92b46ba66cbad9
Jangchaeyun <zzangsally@naver.com>
2025-02-28 10:08:06 +0900

    0228

---
+++ 모든 문제/lv1/Neighboring cells.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<string>> board, int h, int w) {
+	int count = 0;
+	int n = board.size();
+
+	vector<int> dh = { -1, 0, 0, 1 };
+	vector<int> dw = { 0, -1, 1, 0 };
+
+	for (int i = 0; i < 4; i++) {
+		int h_check = h + dh[i];
+		int w_check = w + dw[i];
+
+		if ((0 <= h_check && 0 <= w_check) && (h_check < n && w_check < n)) {
+			if (board[h][w] == board[h_check][w_check])
+				count++;
+		}
+	}
+	return count;
+}
